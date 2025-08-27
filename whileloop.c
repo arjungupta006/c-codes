de43d79a2601b4fc463894fd68bc38c4fc2acf90
arjungupta006 <arjungupta34006@gmail.com>
2025-08-27 12:24:21 +0530

    Create whileloop.c

---
+++ whileloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,d;
+    printf("Enter an int... ");
+    scanf("%d", &n);
+    while (n>0)
+    {
+       d=n%10;
+       n=n/10;
+       printf("%d\n",d);
+    }
+    
+    return 0;
+}
