3c9581d45c0199cccc7c8ef330942bc00be1d2cb
arjungupta006 <arjungupta34006@gmail.com>
2025-09-05 00:52:15 +0530

    Create fullpyramid.c

---
+++ fullpyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int k = 1; k <= n - i; k++) {
+            printf(" ");
+        }
+
+    for (int j = 1; j <= i; j++) {
+            printf("*", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("*", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
