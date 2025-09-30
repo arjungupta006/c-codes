55e16cbf08e25e544d7ca7d71e891086a7150704
arjungupta006 <arjungupta34006@gmail.com>
2025-09-30 20:14:53 +0530

    Create arraypalindrome.c

---
+++ arraypalindrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 1;
+    scanf("%d", &n);
+    int a[n];
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (i = 0; i < n / 2; i++)
+        if (a[i] != a[n - i - 1])
+        {
+            flag = 0;
+            break;
+        }
+    if (flag)
+        printf("Palindrome\n");
+    else
+        printf("Not Palindrome\n");
+    return 0;
+}
+
+
+
+
+
