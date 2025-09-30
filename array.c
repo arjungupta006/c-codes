19a1d1bd50d7cedad7eb5faa99a1cb7f22214ee6
arjungupta006 <arjungupta34006@gmail.com>
2025-09-30 20:16:23 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j = 0;
+    scanf("%d", &n);
+    int a[n], b[n];
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (i = 0; i < n; i++)
+        if (a[i] < 0)
+            b[j++] = a[i];
+    for (i = 0; i < n; i++)
+        if (a[i] >= 0)
+            b[j++] = a[i];
+    for (i = 0; i < n; i++)
+        printf("%d ", b[i]);
+    return 0;
+}
